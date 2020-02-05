dfa4891d3837fe4a9c649842c81bc99fb8f1898b
Sarkisyan <azsarki@USBUR12WB162522s-MacBook-Pro.local>
2020-02-05 00:29:15 -0800

    adding solutions for most of 3.11-3.20

--- ChapterThree.scala
+++ ChapterThree.scala
@@ -1,66 +1,161 @@
-object ChapterThree {
+sealed trait List[+A]
+case object Nil extends List[Nothing]
+case class Cons[+A](head: A, tail: List[A]) extends List[A]
 
-  /* Exercise 3.2 */
-  def tail[A](myList: List[A]): List[A] = myList match {
-    case Nil => List()
-    case x::xs => xs
+object List {
+
+  def sum(ints: List[Int]): Int = ints match { // A function that uses pattern matching to add up a list of integers
+    case Nil => 0 // The sum of the empty list is 0.
+    case Cons(x,xs) => x + sum(xs) // The sum of a list starting with `x` is `x` plus the sum of the rest of the list.
   }
 
-  /* Exercise 3.3 */
-  def setHead[A](myList: List[A], newHead: A): List[A] = myList match {
-    case Nil => List(newHead)
-    case x::xs => newHead::xs
+  def product(ds: List[Double]): Double = ds match {
+    case Nil => 1.0
+    case Cons(0.0, _) => 0.0
+    case Cons(x,xs) => x * product(xs)
   }
 
-  /* Exercise 3.4 */
-  def drop[A](l: List[A], n: Int): List[A] = n match {
-    case 0 => l
-    case x if (x < 0) => l
-    case x => drop(ChapterThree.tail(l), n-1)
+  def apply[A](as: A*): List[A] = // Variadic function syntax
+    if (as.isEmpty) Nil
+    else Cons(as.head, apply(as.tail: _*))
+
+  val x = List(1,2,3,4,5) match {
+    case Cons(x, Cons(2, Cons(4, _))) => x
+    case Nil => 42
+    case Cons(x, Cons(y, Cons(3, Cons(4, _)))) => x + y
+    case Cons(h, t) => h + sum(t)
+    case _ => 101
   }
 
-  /* Exercise 3.5 */
-  def dropWhile[A](l: List[A], f: A => Boolean): List[A] = l match {
-    case Nil => List()
-    case List() => List()
-    case List(x) => List(x)
-    case x::xs if f(x) => dropWhile(xs, f)
-    case _ => l
-  }
+  def append[A](a1: List[A], a2: List[A]): List[A] =
+    a1 match {
+      case Nil => a2
+      case Cons(h,t) => Cons(h, append(t, a2))
+    }
 
-  // Exercise 3.6
+  def foldRight[A,B](as: List[A], z: B)(f: (A, B) => B): B = // Utility functions
+    as match {
+      case Nil => z
+      case Cons(x, xs) => f(x, foldRight(xs, z)(f))
+    }
+
+  def sum2(ns: List[Int]) =
+    foldRight(ns, 0)((x,y) => x + y)
+
+  def product2(ns: List[Double]) =
+    foldRight(ns, 1.0)(_ * _) // `_ * _` is more concise notation for `(x,y) => x * y`; see sidebar
+
+
+  def tail[A](l: List[A]): List[A] =
+    l match {
+      case Nil => sys.error("tail of empty list")
+      case Cons(_,t) => t
+    }
   
+  def setHead[A](l: List[A], h: A): List[A] = l match {
+    case Nil => sys.error("setHead on empty list")
+    case Cons(_,t) => Cons(h,t)
+  }
 
+  def drop[A](l: List[A], n: Int): List[A] =
+    if (n <= 0) l
+    else l match {
+      case Nil => Nil
+      case Cons(_,t) => drop(t, n-1)
+    }
+  
+  def dropWhile[A](l: List[A], f: A => Boolean): List[A] =
+    l match {
+      case Cons(h,t) if f(h) => dropWhile(t, f)
+      case _ => l
+    }
 
+  def init[A](l: List[A]): List[A] =
+    l match {
+      case Nil => sys.error("init of empty list")
+      case Cons(_,Nil) => Nil
+      case Cons(h,t) => Cons(h,init(t))
+    }
+  def init2[A](l: List[A]): List[A] = {
+    import collection.mutable.ListBuffer
+    val buf = new ListBuffer[A]
+    @annotation.tailrec
+    def go(cur: List[A]): List[A] = cur match {
+      case Nil => sys.error("init of empty list")
+      case Cons(_,Nil) => List(buf.toList: _*)
+      case Cons(h,t) => buf += h; go(t)
+    }
+    go(l)
+  }
 
-  /* Exercise 3.9 */
-  def length[A](as: List[A]): Int = {
-    as.foldRight(0)((a: A , b: Int) => 1 + b)
+  def length[A](l: List[A]): Int =
+    foldRight(l, 0)((_,acc) => acc + 1)
+
+  @annotation.tailrec
+  def foldLeft[A,B](l: List[A], z: B)(f: (B, A) => B): B = l match {
+    case Nil => z
+    case Cons(h,t) => foldLeft(t, f(z,h))(f)
+  }
+
+  // 3.11
+  def my_sum(l: List[Int]): Int = l match {
+    case Nil => 0
+    case Cons(x, xs) => x + my_sum(xs)
+  }
+
+  // 3.11
+  def my_prod(l: List[Int]): Int = l match {
+    case Nil => 1
+    case Cons(x, xs) => x * my_prod(xs)
+  }
+
+  // 3.11
+  def my_foldleft_length[A](l: List[A]): Int = {
+    foldLeft(l, 0)((x,_) => x + 1)
+  }
+
+  // 3.12
+  def reverse_with_fold[A](l: List[A]): List[A] = {
+    foldLeft(l, List[A]())((x,y) => Cons(y,x))
+  }
+
+  // 3.14
+  def append_with_foldLeft[A](l: List[A], e: A): List[A] = {
+    foldRight(l, List[A](e))((x,y) => Cons(x,y))
+  }
+
+  // 3.16
+  def addOne(l: List[Int]): List[Int] = {
+    foldRight(l, Nil:List[Int])((x: Int, y) => Cons(x + 1, y))
+  }
+
+  // 3.17
+  def fold_to_string(l: List[Double]): List[String] = {
+    foldRight(l, Nil:List[String])((x: Double, y) => Cons(x.toString, y))
+  }
+
+  // 3.18
+  def my_map[A,B](as: List[A])(f: A => B): List[B] = {
+    foldRight(as, Nil:List[B])((x: A, y) => Cons(f(x), y))
+  }
+
+  // 3.19
+  def filter[A](as: List[A])(f: A => Boolean): List[A] = {
+    
+  }
+
+  def verify[A](a: A, b: A): Unit = {
+    println(a, b, a == b)
   }
 
   def main(args: Array[String]): Unit = {
-    /*
-      Exercise 3.1 - it's the second to last one, so the sum of elements
-    */
-
-    // Exercise 3.2
-    println(ChapterThree.tail(List(1,2,3)))
-
-    // Exercise 3.3
-    println(ChapterThree.setHead(List(1,2,3), 42))
-
-    // Exercise 3.4
-    println(ChapterThree.drop(List(1,2,3,4,5), 3))
-    
-    // 3.5
-    println(ChapterThree.dropWhile(List(1,3,3,4,5,6), (x: Int) => (x % 2 == 1)))
-
-    // 3.6
-
-    // Exercise 3.7
-    // No because the function itself cannot break out of foldRight
-
-    // 3.9
-    println(ChapterThree.length(List(1,2,3,4)))
+    verify(my_sum(List(1,2,3)), 6)
+    verify(my_prod(List(1,2,3)), 6)
+    verify(List.foldLeft(List(1,2,3), 0)((x, _) => x + 1), 3)
+    verify(reverse_with_fold(List(1,2,3)), List(3,2,1))
+    verify(append_with_foldLeft(List(1,2,3), 4), List(1,2,3,4))
+    verify(addOne(List(1,2,3)), List(2,3,4))
+    verify(fold_to_string(List(3.2, 2.1)), List("3.2", "2.1"))
+    verify(my_map(List(1,2,3))((x:Int) => x * 2), List(2,4,6))
   }
 }
